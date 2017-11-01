6ea974d57fe90f59209bb5484a4d0131f6994f5c
chrisjanaqi <25613312+chrisjanaqi@users.noreply.github.com>
2017-11-01 17:32:22 +0100

    added list of song and download links

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const express = require('express');
-const pgp = require('pg-promise');
+const pgp = require('pg-promise')();
 const fs = require('fs');
 
 const app = express();
 
-
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname));
+
+const cn = JSON.parse(fs.readFileSync('./db/conninfo.json'));
+
+const db = pgp(cn);
 
 app.get('/', (req, res) => {
 	res.redirect('/home');
 });
 
 app.get('/home' , (req, res) => {
-	res.render('index');
+    db.any('SELECT * FROM metadata ORDER BY release DESC;')
+        .then(data => {
+            console.log('DATA:', data);
+            res.render('index', {"musics": data});
+        })
+        .catch(error => {
+            console.log('ERROR:', error);
+        });
 });
 
 app.listen(3000, () => {
-	console.log('Example app listening on port 3000!');
+	console.log('App listening on port 3000!');
 });
