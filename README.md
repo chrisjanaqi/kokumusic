b5ca7245203707f0dd0b273607e5e1f02282de25
chrisjanaqi <25613312+chrisjanaqi@users.noreply.github.com>
2017-11-02 01:00:16 +0100

    working on displaying music info (CSS/HTML)

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 This is my personal music website where you will be able to find all the music I've worked on so far available for free listening and download (in format .mp3 .wav and .flac)
 
-## [SoundCloud](https://soundcloud.com/user-468292636)
+## [SoundCloud](https://soundcloud.com/kokumusic)
 
-You can also check them on my [SoundCloud Page](https://soundcloud.com/user-468292636)
+You can also check them on my [SoundCloud Page](https://soundcloud.com/kokumusic)
 
 ## BandCamp
 
@@ -18,11 +18,12 @@ They are already available on my [YouTube channel](https://www.youtube.com/chann
 # TODO List :
 
 - find domain name (1st and 2nd)
-- set a clear database structure
-- link website to database
+- *set a clear database structure => Done!*
+- *link website to database => Done !*
 - dedicated page to update database
 - audio player
-- downloadable links
-- fill database
-- have proper .mp3 .wav and .flac files
+- *downloadable links => Done !*
+- *fill database => Done !*
+- have proper .mp3 .wav and .flac files 
 - get the cover for every music
+- create an engine that convert flac into wav
--- index.js
+++ index.js
@@ -8,6 +8,19 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname));
 
+function formatDate (date) {
+    let month = '' + (date.getMonth() + 1);
+    let day = '' + date.getDate();
+    let year = date.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+
+
 const cn = JSON.parse(fs.readFileSync('./db/conninfo.json'));
 
 const db = pgp(cn);
@@ -16,15 +29,20 @@ app.get('/', (req, res) => {
 	res.redirect('/home');
 });
 
-app.get('/home' , (req, res) => {
+app.get('/home', (req, res) => {
     db.any('SELECT * FROM metadata ORDER BY release DESC;')
         .then(data => {
-            console.log('DATA:', data);
+            //console.log('DATA:', data);
+            for (let music of data) music.release = formatDate(music.release);
             res.render('index', {"musics": data});
         })
         .catch(error => {
             console.log('ERROR:', error);
         });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
 app.listen(3000, () => {
